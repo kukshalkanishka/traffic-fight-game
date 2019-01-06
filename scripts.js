f4ea823b8f2c9ec046774795a3b588ca15044df2
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2019-01-06 13:27:22 +0530

    add car image.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let carPosition = { left: 200, top: 650 };
+let carPosition = { left: 0, top: 500 };
 let roadPosition = { x: 0, y: 0 };
 let score = 0;
 
@@ -33,7 +33,8 @@ const moveRoad = function() {
 const alertIfGameOver = function(obstaclePosition) {
   if (
     obstaclePosition.top == carPosition.top &&
-    obstaclePosition.left == carPosition.left
+    obstaclePosition.left >= carPosition.left &&
+    obstaclePosition.left <= carPosition.left - 30
   ) {
     alert("Game Over");
   }
@@ -48,6 +49,11 @@ const updateObstacle1Position = function() {
       obstacle1Position.top +
       "px"
   );
+  if (obstacle1Position.top >= 600) {
+    clearInterval(obstacle1Id);
+  }
+  console.log(obstacle1Position);
+
   alertIfGameOver(obstacle1Position);
 };
 
@@ -61,6 +67,9 @@ const updateObstacle2Position = function() {
       "px;"
   );
   alertIfGameOver(obstacle2Position);
+  if (obstacle2Position.top >= 600) {
+    clearInterval(obstacle2Id);
+  }
 };
 
 const updateObstacle3Position = function() {
@@ -73,19 +82,24 @@ const updateObstacle3Position = function() {
       "px;"
   );
   alertIfGameOver(obstacle3Position);
+  if (obstacle3Position.top >= 550) {
+    clearInterval(obstacle3Id);
+  }
 };
 
 const generateObstacles = function() {
   obstacle1Position = { left: 0, top: 30 };
   obstacle2Position = { left: 150, top: 0 };
   obstacle3Position = { left: 350, top: 60 };
+
   document.getElementById("road").innerHTML =
     "<div id ='car'></div><div id='obstacle1'></div><div id='obstacle2'></div><div id='obstacle3'></div>";
+
   obstacle1Id = setInterval(updateObstacle1Position, 100);
   obstacle2Id = setInterval(updateObstacle2Position, 200);
   obstacle3Id = setInterval(updateObstacle3Position, 100);
 };
 
 const moveObstacles = function() {
-  setInterval(generateObstacles, 3000);
+  setInterval(generateObstacles, 5000);
 };
